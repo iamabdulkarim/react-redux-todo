cbb39ac85b08f0439a9d07599db849de4c5c00f9
unknown <you@example.com>
2023-03-08 15:04:08 +0600

    todos reducer

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,14 @@
+export const initialState = [
+  {
+    id:1,
+    text:'learn redux',
+    completed:true
+  },
+  {
+    id:2,
+    text: 'learn react with redux',
+    completed:false,
+    color:"red",
+
+  }
+]
